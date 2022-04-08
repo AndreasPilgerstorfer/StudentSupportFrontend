a8b3324c09a7636cd871831cccd1ae061d9b56b5
AndreasPilgerstorfer <71932374+AndreasPilgerstorfer@users.noreply.github.com>
2022-04-08 20:48:00 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ss-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'studentSupportFrontend';
+}
